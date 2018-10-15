40adc7bda2e421e889f38309f3992c52c90ee39a
Tom King <design.tomking@gmail.com>
2018-10-15 14:44:26 -0600

    Initial commit

---
+++ src/utils/apiCalls/index.js
@@ -0,0 +1,18 @@
+const env = process.env.REACT_APP_DATABASE_API_URL;
+
+export const getAllVineyards = () => {
+  const url = env + '/api/v1/vineyards'
+  return fetch(url)
+    .then(res => res.json)
+    .then(data => data)
+    .catch(err => console.log(err));
+}
+
+export const getAllWines = () => {
+  const url = env + '/api/v1/wines'
+  return fetch(url)
+    .then(res => res.json)
+    .then(data => data)
+    .catch(err => console.log(err));
+}
+
