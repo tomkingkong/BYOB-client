454238b74ccbe37612975348a2b414a3c6d98b0e
Tom King <design.tomking@gmail.com>
2018-10-16 14:43:03 -0600

    Create display wines func on wines container

--- src/components/WinesContainer/index.js
+++ src/components/WinesContainer/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import { getAllWines } from '../../utils/apiCalls';
+import { WineCard } from '../WineCard';
 import './Wines.css';
 
 export class WinesContainer extends Component {
@@ -13,13 +14,18 @@ export class WinesContainer extends Component {
 
   async componentDidMount() {
     const wines = await getAllWines();
-    this.setState({ wines });
+    this.setState({ wines: wines.data });
+  }
+
+  displayWines = () => {
+    const { wines } = this.state;
+    return wines.map((wine, i) => <WineCard {...wine} key={i}/>);
   }
 
   render() {
     return (
       <section className="Wines">
-
+        { this.displayWines() }
       </section>
     )
   }
