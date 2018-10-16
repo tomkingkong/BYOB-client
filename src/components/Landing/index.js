fa62cec4af5a663d04dc7b94c83b9524771ca2e8
Tom King <design.tomking@gmail.com>
2018-10-16 08:52:09 -0600

    Create landing page component and test file

---
+++ src/components/Landing/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './landing.css';
+
+export const Landing = () => {
+  return (
+    <main className="landing">
+      <h1 className="title">
+        Sellers Cellars
+      </h1>
+    </main>
+  )
+}
