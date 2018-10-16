dc761c059604d1decfdeaf0438b184daae7654e7
Tom King <design.tomking@gmail.com>
2018-10-16 16:57:23 -0600

    Setup togglewines func for vineyard cards
    Add fetch call to grab all wines
    
    Add conditional rendering too view

--- src/components/VineyardCard/index.js
+++ src/components/VineyardCard/index.js
@@ -1,33 +1,47 @@
 import React, { Component } from 'react';
 import { string, number, func } from 'prop-types';
 
-import { WinesContainer } from '../VineyardsContainer';
+import { getVineyardWines } from '../../utils/apiCalls';
+import { WinesContainer } from '../WinesContainer';
 import './Vineyards.css';
 
 export class VineyardCard extends Component {
   constructor() {
     super();
     this.state = {
-      showWine: false
+      showWine: false,
+      wines: []
     }
   }
 
-  toggleWines = () => {
-    const { displayWines, id } = this.props;
-    displayWines(id);
+  toggleWines = async () => {
+    const { id } = this.props;
+    if (!this.state.wines.length) {
+      let wines = await getVineyardWines(id);
+      this.setState({ wines: wines.data });
+    }
+    this.setState({ showWine: !this.state.showWine });
   }
 
   render() {
     const { name, date_established, location } = this.props;
+    const { showWine, wines } = this.state;
     return (
-      <article className="vineyard">
-        <div className="details">
-          <h2>{name}</h2>
-          <p>{date_established}</p>
-          <p>{location}</p>
-          <button onClick={this.toggleWines}>Wine List</button>
-        </div>
-      </article>
+      <React.Fragment>
+        <article className="vineyard">
+          <div className="details">
+            <h2>{name}</h2>
+            <p>{date_established}</p>
+            <p>{location}</p>
+            <button 
+              className="view-wines" 
+              onClick={this.toggleWines}>
+              View Wine List
+            </button>
+          </div>
+        </article>
+        { showWine && <WinesContainer wines={wines} /> }
+      </React.Fragment>
     )
   }
 }
