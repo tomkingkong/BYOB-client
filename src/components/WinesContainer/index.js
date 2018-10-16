a09f524e1853c1311123da606cdd33923bb66a26
Tom King <design.tomking@gmail.com>
2018-10-16 16:58:28 -0600

    Remove fetch call from winescontainer

--- src/components/WinesContainer/index.js
+++ src/components/WinesContainer/index.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
 
-import { getAllWines } from '../../utils/apiCalls';
 import { WineCard } from '../WineCard';
 import './Wines.css';
 
 export class WinesContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      wines: []
-    }
-  }
-
-  async componentDidMount() {
-    const wines = await getAllWines();
-    this.setState({ wines: wines.data });
-  }
-
   displayWines = () => {
-    const { wines } = this.state;
+    const { wines } = this.props;
     return wines.map((wine, i) => <WineCard {...wine} key={i}/>);
   }
 
