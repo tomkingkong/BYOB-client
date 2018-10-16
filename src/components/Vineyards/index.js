2f8680337b1af2af7834b2943151640a7993c050
Tom King <design.tomking@gmail.com>
2018-10-16 08:52:31 -0600

    Add vineyards component and test file with css

---
+++ src/components/Vineyards/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import { getAllVineyards } from '../../utils/apiCalls';
+import './Vineyards.css';
+
+export class Vineyards extends Component {
+  constructor() {
+    super();
+    this.state = {
+      vineyards: []
+    }
+  }
+
+  async componentDidMount() {
+    const vineyards = await getAllVineyards();
+    this.setState({ vineyards });
+  }
+
+  render() {
+    return (
+      <section className="Vineyards">
+
+      </section>
+    )
+  }
+}
