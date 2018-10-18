a256b1a409bbe6056dc6c43dc69c2a30ce0dccec
Tom King <design.tomking@gmail.com>
2018-10-18 09:29:12 -0600

    Add search feature to vineyard container
    Add function to filter vineyards by name and pass to search component

--- src/components/VineyardsContainer/index.js
+++ src/components/VineyardsContainer/index.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { getAllVineyards } from '../../utils/apiCalls';
 import { VineyardCard } from '../VineyardCard';
 import { VineyardForm } from '../VineyardForm';
+import { SearchVineyards } from '../SearchVineyards';
 import './Vineyards.css';
 
 export class VineyardsContainer extends Component {
@@ -23,15 +24,29 @@ export class VineyardsContainer extends Component {
     return vineyards.map((v, i) => <VineyardCard {...v} key={i} />);
   }  
 
-  addNewVineyard = (vineyard) => {
-    this.setState({ vineyards: [vineyard, ...this.state.vineyards] })
+  addNewVineyard = vineyard => {
+    this.setState({ vineyards: [vineyard, ...this.state.vineyards] });
+  }
+
+  filterVineyards = filter => {
+    const { vineyards } = this.state;
+    const filteredVineyards = vineyards.map(v => {
+      if (!v.name.toLowerCase().includes(filter.toLowerCase())) {
+        v.hide = true;
+      } else {
+        v.hide = false;
+      }
+      return v;
+    });
+    this.setState({ vineyards: filteredVineyards });
   }
 
   render() {
     return (
       <section className="Vineyards">
-        <VineyardForm addNewVineyard={this.addNewVineyard} />
+        <SearchVineyards filterVineyards={this.filterVineyards}/>
         { this.displayVineyards() }
+        <VineyardForm addNewVineyard={this.addNewVineyard} />
       </section>
     )
   }
